7e9b6bb12e8de5bee42551e688fc795045fa0abf
Qualityfy <119132798+qualityfy@users.noreply.github.com>
2022-11-26 22:42:50 +0000

    Update begin.lua

--- begin.lua
+++ begin.lua
@@ -1,1 +1,10 @@
+--NOTE:
+--This script should be placed inside "ServerScriptService" to work properly.
+--This script needs other scripts in other locations, the required location for each script is written as a comment in the respective script.
 
+--List of local variables, such as intervals:
+local waittime = 20 -- The wait time for the next round to begin after the previous ended
+
+local roundtime = 180 -- The time the round takes (60sec (1min) * 3 = 180 (3min))
+
+local min_players = 2 -- The amount of players that have to be in the game for the round to begin (max players is set by the max players in the server)
